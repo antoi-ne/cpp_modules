81c0b018d88db665715044645850d45329619ee8
Antoine Coulon <antoine@30c27b.com>
2021-10-15 19:19:04 +0200

    finished m01/ex05

--- module01/ex05/main.cpp
+++ module01/ex05/main.cpp
@@ -1,4 +1,13 @@
+#include "Karen.hpp"
+
 int main(void)
 {
+	Karen karen;
+
+	karen.complain("DEBUG");
+	karen.complain("INFO");
+	karen.complain("WARNING");
+	karen.complain("ERROR");
+	karen.complain("I'm the manager.");
 	return (0);
 }
