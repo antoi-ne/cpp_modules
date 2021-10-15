b5f1e2db7368b9c414c1ada38d74911a7dff9a42
Antoine Coulon <antoine@30c27b.com>
2021-10-15 16:23:11 +0200

    finished m01/ex02

--- module01/ex02/main.cpp
+++ module01/ex02/main.cpp
@@ -1,4 +1,19 @@
+#include <string>
+#include <iostream>
+
 int main(void)
 {
+	std::string str("HI THIS IS BRAIN");
+
+	std::string* stringPTR = &str;
+	std::string& stringREF = str;
+
+	(void)stringPTR;
+	(void)stringREF;
+
+	std::cout << "str content: " << str << std::endl << "str address: " << (void *)&str << std::endl;
+	std::cout << "stringPTR content: " << *stringPTR << std::endl << "stringPTR address: " << (void *)stringPTR << std::endl;
+	std::cout << "stringREF content: " << stringREF << std::endl << "stringREF address: " << (void *)&stringREF << std::endl;
+
 	return (0);
 }
