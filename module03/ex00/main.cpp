940ee4149c539b9adc50b6e215701e45292b046b
ancoulon <ancoulon@student.s19.be>
2021-10-20 12:48:45 +0200

    finished m03/ex00

--- module03/ex00/main.cpp
+++ module03/ex00/main.cpp
@@ -3,15 +3,19 @@
 int main(void)
 {
 	ClapTrap steve("Steve");
-	ClapTrap steve2;
-
 	steve.attack("Joe");
 	steve.takeDamage(9);
 	steve.takeDamage(2);
 	steve.beRepaired(6);
 	steve.beRepaired(1);
 
+	ClapTrap steve2;
 	steve2 = steve;
-	steve.attack("Joe");
+	steve2.attack("Joe2");
+	steve2.takeDamage(1);
+
+	ClapTrap steve3(steve2);
+	steve2.attack("Joe3");
+	steve3.takeDamage(3);
 	return 0;
 }
