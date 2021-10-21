e92a3dcd2f63d447ffc8b185559a4e9afb1fdda7
ancoulon <ancoulon@student.s19.be>
2021-10-21 08:10:16 +0200

    updated main

--- module02/ex02/main.cpp
+++ module02/ex02/main.cpp
@@ -2,16 +2,33 @@
 
 int main(void)
 {
-	Fixed a;
-	Fixed const b(Fixed(5.05f) * Fixed(2));
+	std::cout << "== Testing from subject ==" << std::endl;
 
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
-	std::cout << Fixed::max(a, b) << std::endl;
+	{
+		Fixed a;
+		Fixed const b(Fixed(5.05f) * Fixed(2));
+
+		std::cout << a << std::endl;
+		std::cout << ++a << std::endl;
+		std::cout << a << std::endl;
+		std::cout << a++ << std::endl;
+		std::cout << a << std::endl;
+		std::cout << b << std::endl;
+		std::cout << Fixed::max(a, b) << std::endl;
+	}
+
+		std::cout << "== Additional testing ==" << std::endl;
+
+	{
+		Fixed a;
+		Fixed const b(Fixed(4.44f) + Fixed(2));
+
+		std::cout << a << std::endl;
+		std::cout << b << std::endl;
+		std::cout << b - Fixed(1) << std::endl;
+		std::cout << b / Fixed(2) << std::endl;
+		std::cout << Fixed::max(a + Fixed(10.02f), b/ Fixed(2)) << std::endl;
+	}
 
 	return 0;
 }
