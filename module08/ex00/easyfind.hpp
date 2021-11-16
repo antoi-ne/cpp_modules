26a05d8286e228cba3c0756bca40575011b30a6d
ancoulon <ancoulon@student.s19.be>
2021-11-16 13:18:21 +0100

    working on m08

--- module08/ex00/easyfind.hpp
+++ module08/ex00/easyfind.hpp
@@ -1,7 +1,26 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <stdexcept>
+#include <algorithm>
+
+class OccurenceNotFoundException: public std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return "Occurence not found in container.";
+				}
+		};
+
 template <typename T>
-void easyfind(T a, int b);
+int & easyfind(T & a, int b)
+{
+	typename T::iterator it;
+	it = std::find(a.begin(), a.end(), b);
+	if (it == a.end())
+		throw OccurenceNotFoundException();
+	return *it;
+}
 
 #endif
--- module08/ex00/main.cpp
+++ module08/ex00/main.cpp
@@ -1,4 +1,21 @@
+#include <easyfind.hpp>
+#include <list>
+#include <iostream>
+#include <stdexcept>
+
 int main(void)
 {
-	return 0;
+	try
+	{
+		std::list<int> lst;
+
+		lst.push_back(4);
+
+		std::cout << easyfind(lst, 1) << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	
 }
